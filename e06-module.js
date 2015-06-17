84064d78ccae8589b534e8a81db63f238b067a36
Karuto <iamkaruto@gmail.com>
2015-06-16 21:59:39 -0700

    Added e06 modules

---
+++ e06-module.js
@@ -0,0 +1,6 @@
+// Export custom module - bind it to a function
+module.exports = test;
+
+function test() {
+	console.log("hi from module");
+}
---
+++ e06.js
@@ -0,0 +1,5 @@
+// Use custom module in Node (module resides in e06-module.js)
+var mymodule = require('./e06-module');
+
+// Call the custom module
+mymodule();
