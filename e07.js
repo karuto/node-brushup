8bd92fd723a0e0e62ca4b66da7531e1607f78b16
Karuto <iamkaruto@gmail.com>
2015-06-23 22:05:25 -0700

    Removed instructions

--- e07.js
+++ e07.js
@@ -1,4 +1,3 @@
-// Write a program that performs an HTTP GET request to a URL provided to you as the first command-line argument. Write the String contents of each "data" event from the response to a new line on the console (stdout).
 var http = require('http');
 
 var url = process.argv[2]; // 1st command line argument will supply URL
